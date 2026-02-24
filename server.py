8bc6ab400789bdeb1dd15dbc83ff9ec69cb732d5
qilichbek01 <qilichbeknormominov@gmail.com>
2026-02-24 20:22:33 +0500

    fix indentation

--- server.py
+++ server.py
@@ -33,3 +33,6 @@ Telefon: {data.get('phone')}
 
     if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+    if __name__ == "__main__":
+    app.run     (host="0.0.0.0", port=5000)
