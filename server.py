cff16373396f7bac84e321823e616ed34bec90e2
qilichbek01 <qilichbeknormominov@gmail.com>
2026-02-24 20:31:15 +0500

    fix duplicate main

--- server.py
+++ server.py
@@ -31,8 +31,5 @@ Telefon: {data.get('phone')}
 
     return jsonify({"status": "ok"})
 
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-    if __name__ == "__main__":
-    app.run     (host="0.0.0.0", port=5000)
+  if __name__ == "__main__":
+         app.run(host="0.0.0.0", port=5000)
