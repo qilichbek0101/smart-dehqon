da2a91819c47a35a7bc096e69ef0cf925323977e
qilichbek01 <qilichbeknormominov@gmail.com>
2026-02-25 23:43:21 +0500

    fix blueprint registration

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from routes.products import products_bp
 from flask import Flask
 from flask_cors import CORS
 from config import Config
 from extensions import db
 from routes.orders import orders_bp
+from routes.products import products_bp
 from models import Product
 
 app = Flask(__name__, static_folder=".", static_url_path="")
@@ -15,7 +15,6 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    # 🔴 seed
     if Product.query.count() == 0:
         db.session.add(Product(name="Pomidor", price=25000, image="/image/pomidor.jpeg"))
         db.session.add(Product(name="Piyoz", price=15000, image="/image/piyoz.jpeg"))
@@ -24,11 +23,8 @@ with app.app_context():
         print(">>> SEEDED")
 
 app.register_blueprint(orders_bp)
+app.register_blueprint(products_bp)
 
 @app.route("/")
 def home():
     return app.send_static_file("index.html")
-
-    from routes.products import products_bp
-app.register_blueprint(products_bp)
-# app.register_blueprint(products_bp)
